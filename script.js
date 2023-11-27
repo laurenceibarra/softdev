00d72b1e3a7af35de9c5bef27a417b4c7bb5e46d
laurenceibarra <laurencemibarra@gmail.com>
2023-11-27 21:32:34 +0800

    Added JS, audio functionality

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// event listener listens for load event (all resources done loading event), then runs function, defined by () => 
-// querySelector selects first element in document with class loader
-// classList.add adds class loader--hidden to loader element
-// addEventListener listens for transitionend event, then runs function
-// transitionend event is when the transition ends for the loader element (transition is the fade out from css) 
-// removeChild removes the loader element from the document body
+document.addEventListener('DOMContentLoaded', function () {
+  const boxes = document.querySelectorAll('.box');
+  let currentAudio = null;
 
-// This line adds an event listener to the 'load' event of the window.
-window.addEventListener("load", () => {
-
-
-    const loader = document.querySelector(".loader");
-    loader.classList.add("loader--hidden");
-    loader.addEventListener("transitionend", () => {
-      document.body.removeChild(loader);
-    });
-    
+  boxes.forEach(box => {
+      box.addEventListener('click', function () {
+          const audioSrc = this.getAttribute('data-src');
+          playAudio(audioSrc);
+      });
   });
 
+  function playAudio(src) {
+      if (currentAudio) {
+          currentAudio.pause();
+      }
 
-
-  
+      const audio = new Audio(src);
+      audio.play();
+      currentAudio = audio;
+  }
+});
